2ece4d588b5d02b7bd88e43c7ae9a0a862f871ed
Shane Matuszek <smatuszeksa@gmail.com>
2019-03-17 17:56:18 +0200

    first commit

--- comprehension/main.py
+++ comprehension/main.py
@@ -5,6 +5,13 @@ class Login:
     def on_get(self, request, response):
         response.body = json.dumps({"status": "OK"})
 
+class OAuth:
+    def on_get(self, request, response):
+        breakpoint()
+    def on_post(self, request, response):
+        breakpoint()
+
 application = falcon.API()
 application.add_route('/login', Login())
+application.add_route('/oauth', OAuth())
 
